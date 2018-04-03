9613e6fcfc18a154de642550e3fb6ead6da27ee0
Camouflage129 <phh129@hanmail.net>
2018-04-03 22:44:38 +0900

    Modify Sql

--- What_Do_I_Sell_Today/ETC/wdist.sql
+++ What_Do_I_Sell_Today/ETC/wdist.sql
@@ -4,66 +4,70 @@ grant dba to wdist;
 connect wdist/1234;
 
 drop table Reply;
-drop table FreeBoard;
-drop table ServiceBoard;
+drop table File;
+drop table FileGroup;
+drop table Board;
 drop table Users;
 
 /**********************************/
-/* Table Name: ServiceBoard */
+/* Table Name: Users */
 /**********************************/
-CREATE TABLE ServiceBoard(
-		Num INT,
+CREATE TABLE Users(
+		UsersID VARCHAR(15),
+		PW VARCHAR(100) NOT NULL,
+		Name VARCHAR(20) NOT NULL,
+		Email VARCHAR(25) NOT NULL
+);
+/**********************************/
+/* Table Name: Board */
+/**********************************/
+CREATE TABLE Board(
+		BoardNum INT,
+		Type VARCHAR(20) NOT NULL,
 		Title VARCHAR(100) NOT NULL,
 		Contents VARCHAR(3000),
-		FileName VARCHAR(100),
-		HashName VARCHAR(100),
-		FileType VARCHAR(10),
-		UserID VARCHAR(15) NOT NULL,
+		UsersID VARCHAR(15) NOT NULL,
 		PostDate DATE NOT NULL
 );
 /**********************************/
-/* Table Name: FreeBoard */
+/* Table Name: FileGroup */
 /**********************************/
-CREATE TABLE FreeBoard(
-		Num INT,
-		Title VARCHAR(100) NOT NULL,
-		Contents VARCHAR(3000),
-		FileName VARCHAR(100),
-		HashName VARCHAR(100),
-		FileType VARCHAR(10),
-		UserID VARCHAR(15) NOT NULL,
-		PostDate DATE NOT NULL
+CREATE TABLE FileGroup(
+		FileGroupNum INT,
+		BoardNum INT
+);
+/**********************************/
+/* Table Name: File */
+/**********************************/
+CREATE TABLE File(
+		FileNum INT,
+		FileName VARCHAR(100) NOT NULL,
+		HashValue VARCHAR(100) NOT NULL,
+		FileGroupNum INT
 );
 /**********************************/
 /* Table Name: Reply */
 /**********************************/
 CREATE TABLE Reply(
-		Num INT,
-		UserID VARCHAR(15) NOT NULL,
+		ReplyNum INT,
+		UsersID VARCHAR(15) NOT NULL,
 		Contents VARCHAR(1000),
 		PostDate DATE NOT NULL,
 		ReplyNum VARCHAR(10),
-		FreeBoardNum INT,
-		ServiceBoardNum INT
-);
-/**********************************/
-/* Table Name: Users */
-/**********************************/
-CREATE TABLE Users(
-		ID VARCHAR(15),
-		PW VARCHAR(100) NOT NULL,
-		Name VARCHAR(20) NOT NULL,
-		Email VARCHAR(25) NOT NULL
+		BoardNum INT
 );
 
-ALTER TABLE Users ADD CONSTRAINT IDX_Users_PK PRIMARY KEY (ID);
+ALTER TABLE Users ADD CONSTRAINT IDX_Users_PK PRIMARY KEY (UsersID);
 
-ALTER TABLE ServiceBoard ADD CONSTRAINT IDX_ServiceBoard_PK PRIMARY KEY (Num);
-ALTER TABLE ServiceBoard ADD CONSTRAINT IDX_ServiceBoard_FK0 FOREIGN KEY (UserID) REFERENCES Users (ID);
+ALTER TABLE Board ADD CONSTRAINT IDX_Board_PK PRIMARY KEY (BoardNum);
+ALTER TABLE Board ADD CONSTRAINT IDX_Board_FK0 FOREIGN KEY (UsersID) REFERENCES Users (UsersID);
 
-ALTER TABLE FreeBoard ADD CONSTRAINT IDX_FreeBoard_PK PRIMARY KEY (Num);
-ALTER TABLE FreeBoard ADD CONSTRAINT IDX_FreeBoard_FK0 FOREIGN KEY (UserID) REFERENCES Users (ID);
+ALTER TABLE Reply ADD CONSTRAINT IDX_Reply_PK PRIMARY KEY (ReplyNum);
+ALTER TABLE Reply ADD CONSTRAINT IDX_Reply_FK0 FOREIGN KEY (BoardNum) REFERENCES Board (BoardNum);
+ALTER TABLE Reply ADD CONSTRAINT IDX_Reply_FK1 FOREIGN KEY (UsersID) REFERENCES Users (UsersID);
 
-ALTER TABLE Reply ADD CONSTRAINT IDX_Reply_PK PRIMARY KEY (Num);
-ALTER TABLE Reply ADD CONSTRAINT IDX_Reply_FK0 FOREIGN KEY (ServiceBoardNum) REFERENCES ServiceBoard (Num);
-ALTER TABLE Reply ADD CONSTRAINT IDX_Reply_FK1 FOREIGN KEY (FreeBoardNum) REFERENCES FreeBoard (Num);
+ALTER TABLE FileGroup ADD CONSTRAINT IDX_FileGroup_PK PRIMARY KEY (FileGroupNum);
+ALTER TABLE FileGroup ADD CONSTRAINT IDX_FileGroup_FK0 FOREIGN KEY (BoardNum) REFERENCES Board (BoardNum);
+
+ALTER TABLE File ADD CONSTRAINT IDX_File_PK PRIMARY KEY (FileNum);
+ALTER TABLE File ADD CONSTRAINT IDX_File_FK0 FOREIGN KEY (FileGroupNum) REFERENCES FileGroup (FileGroupNum);
