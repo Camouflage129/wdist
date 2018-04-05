953a5cb4b8015008a8b98734192d5561a1ef325d
student <student@M90122>
2018-04-05 18:24:52 +0900

    UserController

--- What_Do_I_Sell_Today/src/main/java/com/wdist/web/controller/UserController.java
+++ What_Do_I_Sell_Today/src/main/java/com/wdist/web/controller/UserController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.wdist.biz.user.service.UserService;
@@ -19,7 +20,7 @@ public class UserController {
 	@Resource(name="userService")
 	UserService service;
 	
-	@RequestMapping("")
+	@RequestMapping(value="/signUp.do", method=RequestMethod.POST)
 	public ModelAndView signUpDo(String id, String pw, String name, String email) {
 		Map<String, String> map = new HashMap<String, String>();
 		UserVO vo = new UserVO(id, pw, name, email);
@@ -28,7 +29,17 @@ public class UserController {
 		else
 			map.put("result", "fail");		
 		return new ModelAndView("jsonView", map);
-	}
- 
+	}	
+	
+	@RequestMapping(value="/checkId.do", method=RequestMethod.POST)	
+	public ModelAndView checkIdDo(String id, HttpSession session) {
+		Map<String, String> map = new HashMap<String, String>();
+		if(service.checkId(new UserVO(id,null,null,null)))
+			map.put("result", "success");
+		else
+			map.put("result", "fail");
+		session.setAttribute("checkId", id);		
+		return new ModelAndView("jsonView", map);
+	} 
 	
 }
