a744ada41d4ce0b97c9ed25f6c8323f74eb74a4a
student <student@M90121>
2018-04-20 17:33:43 +0900

    foodcontroller DB

--- What_Do_I_Sell_Today/src/main/java/com/wdist/biz/food/dao/FoodDAO.java
+++ What_Do_I_Sell_Today/src/main/java/com/wdist/biz/food/dao/FoodDAO.java
@@ -1,9 +1,11 @@
 package com.wdist.biz.food.dao;
 
+import com.wdist.biz.food.vo.AreaFoodVO;
 import com.wdist.biz.food.vo.FoodAreaVO;
 
 public interface FoodDAO {
-	//public List<BoardVO> freeOrCsBoard(String Type);
 	
 	public FoodAreaVO getFoodAreaTitle(String foodtitle);
+	
+	public AreaFoodVO getAreaFoodtitle(String areatitle);
 }
