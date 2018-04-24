1e87669109e7d4a910154efe05283418030aa0ae
Camouflage129 <phh129@hanmail.net>
2018-04-24 13:53:31 +0900

    Reply Set

---
+++ What_Do_I_Sell_Today/src/main/java/com/wdist/biz/reply/dao/ReplyDAOImpl.java
@@ -0,0 +1,47 @@
+package com.wdist.biz.reply.dao;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Component;
+
+import com.wdist.biz.board.vo.ReplyVO;
+
+@Component("ReplyDAO")
+public class ReplyDAOImpl implements ReplyDAO{
+	
+	@Resource(name="sqlSessionWindow")
+	private SqlSession sqlSession;
+	
+	@Override
+	public List<ReplyVO> replyList(int BoardNum) {
+		return sqlSession.selectList("replyList", BoardNum);
+	}
+
+	@Override
+	public int insertReply(ReplyVO vo) {
+		return sqlSession.insert("insertReply", vo);
+	}
+
+	@Override
+	public int insertReReply(ReplyVO vo) {
+		return sqlSession.insert("insertReReply", vo);
+	}
+
+	@Override
+	public int modifyParentReply(ReplyVO vo) {
+		return sqlSession.update("modifyParentReply", vo);
+	}
+
+	@Override
+	public int modifyReply(ReplyVO vo) {
+		return sqlSession.update("modifyReply", vo);
+	}
+
+	@Override
+	public int deleteReply(int replyNum) {
+		return sqlSession.delete("deleteReply", replyNum);
+	}
+}
