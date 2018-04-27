f7aeca351dc6b6e0de2bae6b373e9edd674d5e80
Camouflage129 <phh129@hanmail.net>
2018-04-27 14:12:26 +0900

    Setting SFTP

--- What_Do_I_Sell_Today/src/main/java/com/wdist/biz/rserve/RServe.java
+++ What_Do_I_Sell_Today/src/main/java/com/wdist/biz/rserve/RServe.java
@@ -4,7 +4,7 @@ import org.rosuda.REngine.Rserve.RConnection;
 import org.rosuda.REngine.Rserve.RserveException;
 
 public class RServe {
-	public static void main(String[] args) {
+	public void getWordCloud(String species) {
 		RConnection conn = null;
 		try {
 			conn = new RConnection("70.12.109.77",6312);
@@ -17,9 +17,9 @@ public class RServe {
 			conn.eval("library(Rserve)");
 			conn.eval("library(ggplot2)");
 			conn.eval("con <- dbConnect(MySQL(), host = '70.12.109.53', dbname = 'project', user = 'root', password = 'bigdata', port = 3306)");
-			conn.eval("wordcount <- dbGetQuery(con, 'SELECT word, tfidf FROM wordcount WHERE tfidf > (SELECT AVG(tfidf) FROM wordcount) LIMIT 10')");
+			conn.eval("wordcount <- dbGetQuery(con, 'SELECT word, tfidf FROM wordcount WHERE tfidf > (SELECT AVG(tfidf) FROM wordcount)')");
 			conn.eval("palete<-brewer.pal(9,'Set1')");
-			conn.eval("jpeg(filename = '/home/hadoop/rServeTmp/wordcloud4.jpeg',width = 400, height = 400)");
+			conn.eval("jpeg(filename = '/home/hadoop/rServeTmp/"+species+".jpeg',width = 400, height = 400)");
 			conn.eval("wordcloud(wordcount$word, wordcount$tfidf, max.words=Inf, random.order=FALSE, rot.per=.15, colors=palete)");
 			conn.eval("dev.off()");
 			System.out.println("Rserve 수행 끝");
@@ -29,5 +29,22 @@ public class RServe {
 			if( conn != null && conn.isConnected())
 				conn.close();
 		}
+		
+		String host = "70.12.109.77";
+        int port = 22;
+        String userName = "hadoop";
+        String password = "wjdqhqhdks";
+        String dir = "/home/hadoop/rServeTmp/";
+
+        SFTPUtil util = new SFTPUtil();
+        util.init(host, userName, password, port);
+        
+		String fileName=species;
+		String saveDir="C:/Users/User/git/wdist/What_Do_I_Sell_Today/src/main/webapp/image/"+fileName;
+
+        util.download(dir, fileName, saveDir);
+
+        util.disconnection();
+        System.exit(0);
 	}
 }
