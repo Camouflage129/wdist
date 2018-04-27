e1d7b91bc273edad443af001910dcaf6043b058f
Camouflage129 <phh129@hanmail.net>
2018-04-27 11:37:46 +0900

    Rserve, new DataBase, JSONParser.py

---
+++ What_Do_I_Sell_Today/src/main/java/com/wdist/biz/rserve/RServe.java
@@ -0,0 +1,33 @@
+package com.wdist.biz.rserve;
+
+import org.rosuda.REngine.Rserve.RConnection;
+import org.rosuda.REngine.Rserve.RserveException;
+
+public class RServe {
+	public static void main(String[] args) {
+		RConnection conn = null;
+		try {
+			conn = new RConnection("70.12.109.77",6312);
+			System.out.println("RServe 연동, conn: "+conn);
+			conn.eval("library(RColorBrewer)");
+			conn.eval("library(DBI)");
+			conn.eval("library(RMySQL)");
+			conn.eval("library(rJava)");
+			conn.eval("library(wordcloud)");
+			conn.eval("library(Rserve)");
+			conn.eval("library(ggplot2)");
+			conn.eval("con <- dbConnect(MySQL(), host = '70.12.109.53', dbname = 'project', user = 'root', password = 'bigdata', port = 3306)");
+			conn.eval("wordcount <- dbGetQuery(con, 'SELECT word, tfidf FROM wordcount WHERE tfidf > (SELECT AVG(tfidf) FROM wordcount) LIMIT 10')");
+			conn.eval("palete<-brewer.pal(9,'Set1')");
+			conn.eval("jpeg(filename = '/home/hadoop/rServeTmp/wordcloud4.jpeg',width = 400, height = 400)");
+			conn.eval("wordcloud(wordcount$word, wordcount$tfidf, max.words=Inf, random.order=FALSE, rot.per=.15, colors=palete)");
+			conn.eval("dev.off()");
+			System.out.println("Rserve 수행 끝");
+		} catch (RserveException e) {
+			e.printStackTrace();
+		} finally {
+			if( conn != null && conn.isConnected())
+				conn.close();
+		}
+	}
+}
