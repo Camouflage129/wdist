90774509f9ff3be4b4f66073f7c83831b606cda2
student <student@M90121>
2018-04-27 15:44:33 +0900

    jsp

--- What_Do_I_Sell_Today/src/main/java/com/wdist/biz/food/vo/SaleAvgVO.java
+++ What_Do_I_Sell_Today/src/main/java/com/wdist/biz/food/vo/SaleAvgVO.java
@@ -1,5 +1,32 @@
 package com.wdist.biz.food.vo;
 
 public class SaleAvgVO {
-
+	String sale_species; 
+	int asale;
+	
+	
+	public SaleAvgVO() {
+		super();
+	}
+	public SaleAvgVO(String sale_species, int asale) {
+		super();
+		this.sale_species = sale_species;
+		this.asale = asale;
+	}
+	public String getSale_species() {
+		return sale_species;
+	}
+	public void setSale_species(String sale_species) {
+		this.sale_species = sale_species;
+	}
+	public int getAsale() {
+		return asale;
+	}
+	public void setAsale(int asale) {
+		this.asale = asale;
+	}
+	@Override
+	public String toString() {
+		return "SaleAvgVO [sale_species=" + sale_species + ", asale=" + asale + "]";
+	}
 }
